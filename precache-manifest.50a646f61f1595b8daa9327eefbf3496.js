34868c52bd41817972f02910068533519d0461d4
Manoj Basnet <basnetm02@gmail.com>
2022-11-03 09:30:36 +0545

    Updates

---
+++ precache-manifest.50a646f61f1595b8daa9327eefbf3496.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a618fbf2ab7c16ae3edb",
+    "url": "/News-App-React/static/css/main.5a8787ed.chunk.css"
+  },
+  {
+    "revision": "a618fbf2ab7c16ae3edb",
+    "url": "/News-App-React/static/js/main.a618fbf2.chunk.js"
+  },
+  {
+    "revision": "166d73229021a2ada01e",
+    "url": "/News-App-React/static/css/1.60985c96.chunk.css"
+  },
+  {
+    "revision": "166d73229021a2ada01e",
+    "url": "/News-App-React/static/js/1.166d7322.chunk.js"
+  },
+  {
+    "revision": "9da61b4f9c58048c8176",
+    "url": "/News-App-React/static/js/runtime~main.9da61b4f.js"
+  },
+  {
+    "revision": "94c13ee0f921886da273b6176419bf86",
+    "url": "/News-App-React/static/media/loading.94c13ee0.gif"
+  },
+  {
+    "revision": "2b616d8a4eda2b9c96bf4f3d1f414686",
+    "url": "/News-App-React/static/media/background-img.2b616d8a.jpg"
+  },
+  {
+    "revision": "76e28e83afa5cd2084c755fa81b1a797",
+    "url": "/News-App-React/index.html"
+  }
+];
