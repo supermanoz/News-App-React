e71804ce6e42123b898f02f801e374e45ff8a6a1
Manoj Basnet <basnetm02@gmail.com>
2022-11-03 21:15:09 +0545

    Top-Loading-Bar and Infinite Scroll

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
 
 export default class App extends Component {
 
@@ -23,38 +24,44 @@ export default class App extends Component {
     this.state={
       q:null,
       refQ:React.createRef(),
+      progress:30,
+      apiKey:"6a9c5e1ffa9040828da980b65aaf3809",
+      //apiKey:"b428bf1a4fde49b1905f94f4a2f86dbc"
     }
+  }
+
+  setProgress=(progress)=>{
+    this.setState({progress:progress})
   }
 
   render() {
     return (
       <Router>
         <div className="container main">
-          <Navbar projectTitle="Supermanoz News" onSearch={this.handleSearch}/>
+          <Navbar projectTitle="Supermanoz News" onSearch={this.handleSearch}/> 
+          <LoadingBar 
+          color='linear-gradient(90deg, #FAD961 0%, #F76B1C 100%)' progress={this.state.progress} height={3} onLoaderFinished={() => this.setProgress(0)}/>
           <div className="container secondary">
             <Routes>
-              <Route exact path="/" element={<News key="general" pageSize={7} ref={this.state.refQ} q={this.state.q}/>}></Route>
-              <Route exact path="/business" element={<News key="business" pageSize={7} category="business" ref={this.state.refQ} q={this.state.q}/>}></Route>
-              <Route exact path="/entertainment" element={<News key="entertainment" pageSize={7} category="entertainment" ref={this.state.refQ} q={this.state.q}/>}></Route>
-              <Route exact path="/health" element={<News key="health" pageSize={7} category="health" ref={this.state.refQ} q={this.state.q}/>}></Route>
-              <Route exact path="/science" element={<News key="science" pageSize={7} category="science" ref={this.state.refQ} q={this.state.q}/>}></Route>
-              <Route exact path="/sports" element={<News key="sports" pageSize={7} category="sports" ref={this.state.refQ} q={this.state.q}/>}></Route>
-              <Route exact path="/technology" element={<News key="technology" pageSize={7} category="technology" ref={this.state.refQ} q={this.state.q}/>}></Route>
-              <Route exact path="/ca" element={<News key="ca" pageSize={7} country="ca" ref={this.state.refQ} q={this.state.q}/>}></Route>
-              <Route exact path="/cn" element={<News key="cn" pageSize={7} country="cn" ref={this.state.refQ} q={this.state.q}/>}></Route>
-              <Route exact path="/fr" element={<News key="fr" pageSize={7} country="fr" ref={this.state.refQ} q={this.state.q}/>}></Route>
-              <Route exact path="/hk" element={<News key="hk" pageSize={7} country="hk" ref={this.state.refQ} q={this.state.q}/>}></Route>
-              <Route exact path="/in" element={<News key="in" pageSize={7} country="in" ref={this.state.refQ} q={this.state.q}/>}></Route>
-              <Route exact path="/jp" element={<News key="jp" pageSize={7} country="jp" ref={this.state.refQ} q={this.state.q}/>}></Route>
-              <Route exact path="/kr" element={<News key="kr" pageSize={7} country="kr" ref={this.state.refQ} q={this.state.q}/>}></Route>
-              <Route exact path="/pt" element={<News key="pt" pageSize={7} country="pt" ref={this.state.refQ} q={this.state.q}/>}></Route>
-              <Route exact path="/ru" element={<News key="ru" pageSize={7} country="ru" ref={this.state.refQ} q={this.state.q}/>}></Route>
-              <Route exact path="/us" element={<News key="us" pageSize={7} country="us" ref={this.state.refQ} q={this.state.q}/>}></Route>
+              <Route exact path="/" element={<News onHandleProgress={this.setProgress} key="general" pageSize={7} ref={this.state.refQ} q={this.state.q} apiKey={this.state.apiKey}/>}></Route>
+              <Route exact path="/business" element={<News onHandleProgress={this.setProgress} key="business" pageSize={7} category="business" ref={this.state.refQ} q={this.state.q} apiKey={this.state.apiKey}/>}></Route>
+              <Route exact path="/entertainment" element={<News onHandleProgress={this.setProgress} key="entertainment" pageSize={7} category="entertainment" ref={this.state.refQ} q={this.state.q} apiKey={this.state.apiKey}/>}></Route>
+              <Route exact path="/health" element={<News onHandleProgress={this.setProgress} key="health" pageSize={7} category="health" ref={this.state.refQ} q={this.state.q} apiKey={this.state.apiKey}/>}></Route>
+              <Route exact path="/science" element={<News onHandleProgress={this.setProgress} key="science" pageSize={7} category="science" ref={this.state.refQ} q={this.state.q} apiKey={this.state.apiKey}/>}></Route>
+              <Route exact path="/sports" element={<News onHandleProgress={this.setProgress} key="sports" pageSize={7} category="sports" ref={this.state.refQ} q={this.state.q} apiKey={this.state.apiKey}/>}></Route>
+              <Route exact path="/technology" element={<News onHandleProgress={this.setProgress} key="technology" pageSize={7} category="technology" ref={this.state.refQ} q={this.state.q} apiKey={this.state.apiKey}/>}></Route>
+              <Route exact path="/ca" element={<News onHandleProgress={this.setProgress} key="ca" pageSize={7} country="ca" ref={this.state.refQ} q={this.state.q} apiKey={this.state.apiKey}/>}></Route>
+              <Route exact path="/cn" element={<News onHandleProgress={this.setProgress} key="cn" pageSize={7} country="cn" ref={this.state.refQ} q={this.state.q} apiKey={this.state.apiKey}/>}></Route>
+              <Route exact path="/fr" element={<News onHandleProgress={this.setProgress} key="fr" pageSize={7} country="fr" ref={this.state.refQ} q={this.state.q} apiKey={this.state.apiKey}/>}></Route>
+              <Route exact path="/hk" element={<News onHandleProgress={this.setProgress} key="hk" pageSize={7} country="hk" ref={this.state.refQ} q={this.state.q} apiKey={this.state.apiKey}/>}></Route>
+              <Route exact path="/in" element={<News onHandleProgress={this.setProgress} key="in" pageSize={7} country="in" ref={this.state.refQ} q={this.state.q} apiKey={this.state.apiKey}/>}></Route>
+              <Route exact path="/jp" element={<News onHandleProgress={this.setProgress} key="jp" pageSize={7} country="jp" ref={this.state.refQ} q={this.state.q} apiKey={this.state.apiKey}/>}></Route>
+              <Route exact path="/kr" element={<News onHandleProgress={this.setProgress} key="kr" pageSize={7} country="kr" ref={this.state.refQ} q={this.state.q} apiKey={this.state.apiKey}/>}></Route>
+              <Route exact path="/pt" element={<News onHandleProgress={this.setProgress} key="pt" pageSize={7} country="pt" ref={this.state.refQ} q={this.state.q} apiKey={this.state.apiKey}/>}></Route>
+              <Route exact path="/ru" element={<News onHandleProgress={this.setProgress} key="ru" pageSize={7} country="ru" ref={this.state.refQ} q={this.state.q} apiKey={this.state.apiKey}/>}></Route>
+              <Route exact path="/us" element={<News onHandleProgress={this.setProgress} key="us" pageSize={7} country="us" ref={this.state.refQ} q={this.state.q} apiKey={this.state.apiKey}/>}></Route>
             </Routes>
           </div>
-        </div>
-        <div className="text-center m-4">
-            <small>Disclaimer: This site does not store any articles on its server. All contents are provided by News API.</small>
         </div>
       </Router>
     )
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -4,6 +4,7 @@ import Carousel from "./Carousel";
 import Introduction from "./Introduction";
 import Loading from "./Loading";
 import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 export default class News extends Component {
   static defaultProps = {
@@ -26,83 +27,59 @@ export default class News extends Component {
     super();
     this.state = {
       articles: this.newsArr,
-      loading: false,
       page: 1,
       totalResults: 0,
       q: null,
     };
   }
 
-  async componentDidMount() {
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=b428bf1a4fde49b1905f94f4a2f86dbc&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+  async componentDidMount(props) {
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.state.q
-      ? (url = `https://newsapi.org/v2/everything?q=${this.state.q}&apiKey=b428bf1a4fde49b1905f94f4a2f86dbc&pageSize=${this.props.pageSize}&page=${this.state.page}`)
+      ? (url = `https://newsapi.org/v2/everything?q=${this.state.q}&apiKey=${this.props.apiKey}&pageSize=${this.props.pageSize}&page=${this.state.page}`)
       : (url += "");
-    this.setState({
-      loading: true,
-    });
     let data = await fetch(url);
+    this.props.onHandleProgress(50);
     let parsedData = await data.json();
+    this.props.onHandleProgress(70);
     let articles = parsedData.articles;
     this.setState({
       articles: articles,
       totalResults: parsedData.totalResults,
-      loading: false,
     });
-    console.log(url);
+    // console.log(url);
+    this.props.onHandleProgress(100);
+    document.title =
+      "Supermanoz News - " +
+      this.props.category.slice(0, 1).toUpperCase() +
+      this.props.category.slice(1, this.props.category.length);
   }
 
-  handlePrevious = async () => {
-    this.setState({
-      page: this.state.page - 1,
-    });
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=b428bf1a4fde49b1905f94f4a2f86dbc&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+  fetchMoreData = async () => {
+    this.setState({ page: this.state.page + 1 });
+    let url = `https://newsapi.org/v2/top-headlines?country=${
+      this.props.country
+    }&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${
+      this.state.page + 1
+    }&pageSize=${this.props.pageSize}`;
     this.state.q
-      ? (url = `https://newsapi.org/v2/everything?q=${this.state.q}&apiKey=b428bf1a4fde49b1905f94f4a2f86dbc&pageSize=${this.props.pageSize}&page=${this.state.page}`)
+      ? (url = `https://newsapi.org/v2/everything?q=${this.state.q}&apiKey=${
+          this.props.apiKey
+        }&pageSize=${this.props.pageSize}&page=${this.state.page + 1}`)
       : (url += "");
-    this.setState({
-      loading: true,
-    });
     let data = await fetch(url);
     let parsedData = await data.json();
-    let articles = parsedData.articles;
+    let articles = this.state.articles.concat(parsedData.articles);
     this.setState({
       articles: articles,
-      loading: false,
       totalResults: parsedData.totalResults,
     });
-  };
-
-  handleNext = async () => {
-    this.setState({
-      page: this.state.page + 1,
-    });
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=b428bf1a4fde49b1905f94f4a2f86dbc&page=${this.state.page}&pageSize=${this.props.pageSize}`;
-    this.state.q
-      ? (url = `https://newsapi.org/v2/everything?q=${this.state.q}&apiKey=b428bf1a4fde49b1905f94f4a2f86dbc&pageSize=${this.props.pageSize}&page=${this.state.page}`)
-      : (url += "");
-    this.setState({
-      loading: true,
-    });
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    let articles = parsedData.articles;
-    this.setState({
-      articles: articles,
-      loading: false,
-      totalResults: parsedData.totalResults,
-    });
+    // console.log(this.state.page + " " + url);
   };
 
   render() {
     return (
       <>
-        {/* <button
-          className="btn btn-outline-light"
-          onClick={() => this.componentDidMount()}
-        >
-          Search
-        </button> */}
         <div className="container my-2">
           <div className="row">
             <div className="col-lg-5 col-sm-12 my-4">
@@ -112,58 +89,54 @@ export default class News extends Component {
               <Introduction />
             </div>
           </div>
-          <div className="row py-4">
-            <h1 className="my-4 text-center text-white">Top Headlines</h1>
-            <hr className="my-4 text-white" />
-            {this.state.loading && <Loading />}
-            {!this.state.loading &&
-              this.state.articles.map((news) => {
-                if (
-                  news.title &&
-                  news.description &&
-                  news.url &&
-                  news.urlToImage
-                )
-                  return (
-                    <div
-                      className="col-lg-4 col-md-6 col-sm-12 my-4 d-flex justify-content-center"
-                      key={news.url}
-                    >
-                      <NewsItem
-                        urlToImage={news.urlToImage}
-                        title={news.title}
-                        description={news.description}
-                        url={news.url}
-                        author={news.author}
-                        source={news.source}
-                        publishedAt={news.publishedAt}
-                      />
-                    </div>
-                  );
-                else return "";
-              })}
-          </div>
+          <InfiniteScroll
+            dataLength={this.state.articles.length}
+            next={this.fetchMoreData}
+            hasMore={this.state.articles.length != this.state.totalResults}
+            loader={<Loading />}
+          >
+            <div className="container-fluid">
+              <div className="row py-4">
+                <h1 className="my-4 text-center text-white">Top Headlines</h1>
+                <hr className="my-4 text-white" />
+                {this.state.articles.map((news) => {
+                  if (
+                    news.title &&
+                    news.description &&
+                    news.url &&
+                    news.urlToImage
+                  )
+                    return (
+                      <div
+                        className="col-lg-4 col-md-6 col-sm-12 my-4 d-flex justify-content-center"
+                        key={news.url}
+                      >
+                        <NewsItem
+                          urlToImage={news.urlToImage}
+                          title={news.title}
+                          description={news.description}
+                          url={news.url}
+                          author={news.author}
+                          source={news.source}
+                          publishedAt={news.publishedAt}
+                        />
+                      </div>
+                    );
+                  else return "";
+                })}
+              </div>
+            </div>
+          </InfiniteScroll>
         </div>
         <div className="container transparent p-4">
           <div className="row">
-            <div className="col d-flex justify-content-between m-4">
-              <button
-                className="btn btn-md btn-light btn-rounded"
-                onClick={this.handlePrevious}
-                disabled={this.state.page <= 1}
-              >
-                &larr; Previous
-              </button>
-              <button
-                className="btn btn-md btn-light btn-rounded"
-                onClick={this.handleNext}
-                disabled={
-                  this.state.page + 1 >
-                  Math.ceil(this.state.totalResults / this.props.pageSize)
-                }
-              >
-                Next &rarr;
-              </button>
+            <div className="col d-flex justify-content-center m-4">
+              <div className="text-center">
+                <small>
+                  Disclaimer: This site does not store any articles on its
+                  server. All contents are provided by News API.
+                </small>
+              </div>
             </div>
           </div>
         </div>
